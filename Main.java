34aef27ce4de3fc6150386a7eed094bf40900f97
Nidhi <nidhisahani99@gmail.com>
2024-05-26 11:33:48 +0530

    first commmit

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("This is my java file");
+        System.out.println("Nidhi Sahani");
+        System.out.println(new Date());
+    }
+}
