3ca25687fc8865a14ea826dadc522d8b56c8e1aa
Nidhi <nidhisahani99@gmail.com>
2024-05-26 14:51:09 +0530

    fun 2 commit

--- Main.java
+++ Main.java
@@ -4,7 +4,8 @@ public class Main{
     public static void main(String[] args) {
         System.out.println("This is my java file");
         System.out.println("Nidhi Sahani");
-        System.out.println(new Date());
-        System.out.println("Today is a new day");
+ 
+	System.out.println(new Date());
+        System.out.println("Today is a new day,Im enjoying a lot.");
     }
 }
